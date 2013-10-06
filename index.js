b1e8d02922077869a5cb431f74ab8c4160ea22f4
Rebecca Murphey <rmurphey@gmail.com>
2013-10-06 16:56:15 -0400

    sort of working

--- index.js
+++ index.js
@@ -4,7 +4,31 @@ var fs = require('fs-extra');
 var pics = [];
 
 var limit = 5;
-var interval = 1 * 60 * 1000; // 1 minute
+var interval = 1 * 10 * 1000; // 1 minute
+var imagesDir = __dirname + '/public/img/';
+
+console.log(imagesDir);
+
+fs.readdir(imagesDir, function (err, files) {
+  console.log('got here');
+
+  if (err) {
+    console.log('error reading old images', err);
+    return;
+  }
+
+  console.log(files);
+
+  files.forEach(function (file) {
+    if (file.match(/\.jpg$/)) {
+      pics.unshift({
+        src : '/public/img/' + file
+      });
+    }
+  });
+
+  console.log(pics);
+});
 
 app.use(express.logger());
 app.use('/public', express.static(__dirname + '/public'));
@@ -20,15 +44,17 @@ app.engine('jade', require('jade').__express);
 
 function cleanup () {
   if (pics.length > limit) {
-    var oldest = pics.shift();
-    fs.remove(oldest, function (err) {
+    fs.remove(pics.shift(), function (err) {
       if (err) {
         console.log('error removing file', oldest, err);
         return;
       }
 
+      console.log('cleanup complete');
       setTimeout(takePicture, interval);
     });
+  } else {
+    setTimeout(takePicture, interval);
   }
 }
 
@@ -37,15 +63,22 @@ function takePicture () {
   var filename = '/img/olivercam-' + new Date().getTime() + '.jpg';
   var fullFilename = __dirname + '/public' + filename
 
+  console.log('taking a picture', filename);
+
   exec('fswebcam -r 640*480 -d /dev/video0 ' + fullFilename, function (err, stdout, stderr) {
     if (err) {
       console.log('error taking picture', err);
       return;
     }
 
-    pics.push({ src : filename });
+    console.log('took a picture');
+
+    pics.push({ src : '/public' + filename });
     cleanup();
   });
 }
 
 app.listen(3000);
+takePicture();
+
+console.log('app started on localhost:3000');
